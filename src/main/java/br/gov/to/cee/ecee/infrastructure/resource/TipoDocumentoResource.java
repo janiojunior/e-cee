780cb1796af61625e02d92e575474d889000d401
Janio <janiojunior@gmail.com>
2024-09-09 23:12:34 -0300

    ajuste

--- src/main/java/br/gov/to/cee/ecee/infrastructure/resource/TipoDocumentoResource.java
+++ src/main/java/br/gov/to/cee/ecee/infrastructure/resource/TipoDocumentoResource.java
@@ -20,7 +20,7 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-@Path("/tipodocumento")
+@Path("/tipodocumentos")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class TipoDocumentoResource {
